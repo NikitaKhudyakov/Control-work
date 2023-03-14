3af4ead1a43af8eaaf294115f56a10ffc59c725a
Nikita khudyakov <xomnik@gmail.com>
2023-03-14 18:47:13 +0300

    Итоговый код

--- task/Program.cs
+++ task/Program.cs
@@ -5,26 +5,26 @@ string[] array = CountingSymbol(lengthArray, index);
 
 string[] CountingSymbol(int lengthArray, int index)
 {
-string[] array = new string[lengthArray];
-for (int i = 0; i < lengthArray; i++)
-{
-Console.WriteLine("Введите cимвол!");
-string symbol = Console.ReadLine();
-int lengthA = symbol.Length;
-if (lengthA <= 3)
-{
-for (int j = index; j < array.Length; j++)
-{
-array[j] = symbol;
-}
-index++;
-}
-}
-
-Console.WriteLine();
-for (int j = 0; j < index; j++)
-{
-Console.Write($"{array[j]} ");
-}
-return array;
+    string[] array = new string[lengthArray];
+    for (int i = 0; i < lengthArray; i++)
+    {
+        Console.WriteLine("Введите cимвол!");
+        string symbol = Console.ReadLine();
+        int lengthA = symbol.Length;
+        if (lengthA <= 3)
+        {
+            for (int j = index; j < array.Length; j++)
+            {
+                array[j] = symbol;
+            }
+            index++;
+        }
+    }
+    
+    Console.WriteLine();
+    for (int d = 0; d < index; d++)
+    {
+        Console.Write($"{array[d]} ");
+    }
+    return array;
 }
